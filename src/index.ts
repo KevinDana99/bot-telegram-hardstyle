996150b06b9b87e1b043d9ed85671550ff60769e
Kevin <kevindana99@gmail.com>
2026-01-14 19:27:20 -0300

    init repository

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { Telegraf } from "telegraf";
+const BOT_TOKEN = process.env.ACCESS_TOKEN_TELEGRAM_BOT;
+
+if (BOT_TOKEN === undefined || BOT_TOKEN === "") {
+  throw new Error("BOT_TOKEN no encontrado. Revisá tu archivo .env");
+}
+export const bot = new Telegraf(BOT_TOKEN);
